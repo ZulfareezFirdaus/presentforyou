3d9c9eb70f3175b93430ab82055658858e60a131
Zulfareez Firdaus <77760824+ZulfareezFirdaus@users.noreply.github.com>
2024-07-25 23:54:04 +0800

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ envelope.addEventListener('click', () => {
   envelope.classList.toggle('flap');
     
     setTimeout(() => {
-      window.location.href = './foryou/index.html'; // replace with your target URL
+      window.location.href = '/foryou/index.html'; // replace with your target URL
     }, 5000); // 10 seconds
 });
 
